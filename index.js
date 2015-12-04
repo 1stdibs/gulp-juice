6bb98ddc665da696e122a2b1a340e2a5a6f696a7
Brett Ashford <brettjashford@gmail.com>
2015-12-04 17:25:01 -0500

    refactor to support juice 1.8

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 var es = require('event-stream'),
     gutil = require('gulp-util'),
-    juiceContent = require('juice').juiceContent,
+    juice = require('juice'),
     File = gutil.File,
     PluginError = gutil.PluginError,
     crypto = require('crypto');
 
 module.exports = function(options){
-  options = options || {};
-  return es.map(function(file, fn){
-      options.url = 'file://' + file.path;
-      juiceContent(file.contents, options, function(err, html){
-        if(err) return fn(err);
-        file.contents = new Buffer(html);
+    options = options || {};
+    return es.map(function(file, fn){
+        options.url = 'file://' + file.path;
+        file.contents = new Buffer(juice(file.contents.toString(), options));
         fn(null, file);
-      });
     });
 };
